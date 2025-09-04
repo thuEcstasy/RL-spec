3e40d13d9a48c671bf43254c4173d621e1ffb5ec
Lanxiang Hu <hlxde1@yahoo.com>
2025-09-04 21:14:17 +0000

    update profiling scripts & multiblock inference scripts

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -6,4 +6,4 @@ pip uninstall torch -y && pip install torch --index-url https://download.pytorch
 
 python3.10 -m venv cllm2
 source cllm2/bin/activate
-pip install -r requirements.txt
+pip install -r requirements_gb200.txt
